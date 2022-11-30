3c35d71402a9e9d9812a6dcbdbe14c4d389bc0c9
Nathan Fontenele <nathangf60@outlook.com>
2022-11-30 20:09:55 -0300

    Alteração na composição de classes

--- bytebank/bytebank/Contas/ContaCorrente.cs
+++ bytebank/bytebank/Contas/ContaCorrente.cs
@@ -1,63 +1,60 @@
-﻿using System;
+﻿using bytebank.Titular;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace bytebank
+namespace bytebank.Contas
 {
     public class ContaCorrente
     {
         public int numero_agencia;
         public string conta;
-        public string titular;
-        public string titularCPF;
-        public string titularProfissao;
+        public Cliente titular; //recebe a referência de Cliente
         public double saldo = 100; //define o valor padrão do atributo
 
         public void Depositar(double valor)
         {
-            this.saldo += valor;
+            saldo += valor;
         }
-
         public bool Sacar(double valor)
         {
-            if (valor <= this.saldo)
+            if (valor <= saldo)
             {
-                this.saldo -= valor;
+                saldo -= valor;
                 return true;
             }
             else
             {
                 return false;
             }
-            
-        }
 
+        }
         public bool Transferir(double valor, ContaCorrente destino)
         {
-            if(this.saldo < valor) 
+            if (saldo < valor)
             {
                 return false;
             }
             else
             {
-                this.Sacar(valor); //this.saldo -= valor;
+                Sacar(valor); //this.saldo -= valor;
                 destino.Depositar(valor); //destino.saldo += valor;
+                Console.WriteLine("Saldo após a transferencia:");
+                Console.WriteLine($"Saldo do André: {saldo}");
+                Console.WriteLine($"Saldo do Maria: {destino.saldo}");
                 return true;
             }
         }
-
         public void ExibeInformacoes()
         {
-            Console.WriteLine($"Titular da conta: {titular}");
+            Console.WriteLine($"Titular da conta: {titular.nome}");
+            Console.WriteLine($"Profissão: {titular.profissao}");
+            Console.WriteLine($"CPF: {titular.CPF}");
             Console.WriteLine($"Número da agência: {numero_agencia}");
             Console.WriteLine($"Número da conta {conta}");
             Console.WriteLine($"Saldo da conta: {saldo}\n");
         }
-
-
     }
-
-
 }
--- bytebank/bytebank/Program.cs
+++ bytebank/bytebank/Program.cs
@@ -1,42 +1,61 @@
-﻿using bytebank;
+﻿using bytebank.Contas;
+using bytebank.Titular;
 
-ContaCorrente contaAndre = new ContaCorrente();
-contaAndre.titular = "André silva";
-contaAndre.numero_agencia = 15;
-contaAndre.conta = "1010-x";
-contaAndre.saldo = 100;
+//ContaCorrente contaAndre = new ContaCorrente();
+//contaAndre.titular = "André silva";
+//contaAndre.numero_agencia = 15;
+//contaAndre.conta = "1010-x";
+//contaAndre.saldo = 100;
 
-ContaCorrente contaAndre2 = new ContaCorrente();
-contaAndre2.titular = "André silva";
-contaAndre2.numero_agencia = 15;
-contaAndre2.conta = "1010-x";
-contaAndre2.saldo = 100;
+//ContaCorrente contaAndre2 = new ContaCorrente(); //teste para passagens por valor e referência
+//contaAndre2.titular = "André silva";
+//contaAndre2.numero_agencia = 15;
+//contaAndre2.conta = "1010-x";
+//contaAndre2.saldo = 100;
 
-ContaCorrente contaMaria = new ContaCorrente();
-contaMaria.titular = "Maria Souza";
-contaMaria.numero_agencia = 17;
-contaMaria.conta = "1010-5";
-contaMaria.saldo = 350;
+//ContaCorrente contaMaria = new ContaCorrente();
+//contaMaria.titular = "Maria Souza";
+//contaMaria.numero_agencia = 17;
+//contaMaria.conta = "1010-5";
+//contaMaria.saldo = 350;
 
-ContaCorrente contaPedro = new ContaCorrente();
+//ContaCorrente contaPedro = new ContaCorrente(); //teste para imprimir valores padrões
 
-//passagem pro referência
-Console.WriteLine(contaAndre == contaAndre2); // false - os espaços de memórias são diferentes
+////passagem pro referência
+//Console.WriteLine(contaAndre == contaAndre2); // false - os espaços de memórias são diferentes
 
-//passagem por valor
-contaAndre = contaAndre2;
-Console.WriteLine(contaAndre == contaAndre2); //true - aponta para o mesmo objeto
+////passagem por valor
+//contaAndre = contaAndre2;
+//Console.WriteLine(contaAndre == contaAndre2); //true - aponta para o mesmo objeto
 
-contaAndre.ExibeInformacoes();
-contaMaria.ExibeInformacoes();
-contaPedro.ExibeInformacoes();
+//contaAndre.ExibeInformacoes();
+//contaMaria.ExibeInformacoes();
+//contaPedro.ExibeInformacoes();
 
-contaAndre.Transferir(50, contaMaria); //transfere da conta do andre para conta da maria
-Console.WriteLine("Saldo após a transferencia:");
-Console.WriteLine($"Saldo do André: {contaAndre.saldo}");
-Console.WriteLine($"Saldo do Maria: {contaMaria.saldo}");
+//contaAndre.Transferir(50, contaMaria); //transfere da conta do andre para conta da maria
 
+Cliente cliente = new Cliente();
+cliente.nome = "André Souza";
+cliente.CPF = "123456789";
+cliente.profissao = "Analista";
 
+ContaCorrente conta = new ContaCorrente();
+conta.titular = cliente;
+conta.conta = "1010-x";
+conta.numero_agencia = 15;
+conta.saldo = 100;
 
+conta.ExibeInformacoes();
+
+ContaCorrente conta2 = new ContaCorrente();
+conta2.titular = new Cliente(); //referência ao objeto titular
+conta2.titular.nome = "José Souza";
+conta2.titular.profissao = "Tester";
+conta2.titular.CPF = "234567891";
+conta2.conta = "999-x";
+conta2.numero_agencia = 18;
+conta2.saldo = 500;
+
+conta2.ExibeInformacoes();
 
 
