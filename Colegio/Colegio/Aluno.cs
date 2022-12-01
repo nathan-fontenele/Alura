51bf069491dd3ee36868c6b8e381950bea96f8df
Nathan Fontenele <nathangf60@outlook.com>
2022-11-30 22:03:02 -0300

    Uso de propriedades

---
+++ Colegio/Colegio/Aluno.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colegio
+{
+    public class Aluno
+    {
+        private int matricula;
+        public int Matricula
+        {
+            get
+            {
+                return matricula;
+            }
+            set
+            {
+                if(value <= 0)
+                {
+                    Console.WriteLine("Valor inválido, deve ser maior que 0");
+                }
+                else
+                {
+                    matricula = value;
+                }
+            }
+        }
+        private string nome;
+        private string email;
+        private string status;
+        private string tipoCurso;
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public bool Status { get; set; }
+        public int TipoCurso { get; set; }
+    }
+}
---
+++ Colegio/Colegio/Program.cs
@@ -0,0 +1,22 @@
+﻿using Colegio;
+using System;
+
+class Program
+{
+    public static void Main (string[] args)
+    {
+        Aluno aluno = new Aluno();
+
+        aluno.Matricula = 23;
+        aluno.Nome = "Marcelo Oliveira";
+        aluno.Email = "marcelo.oliveira@gmail.com";
+        aluno.Status = true;
+        aluno.TipoCurso = 1;
+
+        Console.WriteLine($"Matricula: {aluno.Matricula}");
+        Console.WriteLine($"Nome: {aluno.Nome}");
+        Console.WriteLine($"E-mail: {aluno.Email}");
+        Console.WriteLine($"Status: {aluno.Status}");
+        Console.WriteLine($"Tipo do curso: {aluno.TipoCurso}");
+    }
+}
