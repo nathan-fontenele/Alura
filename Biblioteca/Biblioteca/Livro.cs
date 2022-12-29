26324916f4ec548e26e720f54ed84d67e79386be
Nathan Fontenele <nathangf60@outlook.com>
2022-12-28 22:13:25 -0300

    Criação do projeto biblioteca

---
+++ Biblioteca/Biblioteca/Livro.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Livro
+    {
+        public string Titulo { get; private set; }
+        public string Autor { get; set; }
+        public string ISBN { get; private set; }
+        public int Ano { get; set; }
+        public string Estilo { get; set; }
+        public int Paginas { get; set; }
+        public string Pais { get; set; }
+        public Livro(string titulo, string isbn) 
+        {
+            this.Titulo = titulo;
+            this.ISBN = isbn;
+        }
+
+        public void ExibeInformacoes()
+        {
+            Console.WriteLine($"Título {Titulo}");
+            Console.WriteLine($"Autor {Autor}");
+            Console.WriteLine($"ISBN {ISBN}");
+            Console.WriteLine($"Ano {Ano}");
+            Console.WriteLine($"Páginas {Paginas}");
+            Console.WriteLine($"Estilo {Estilo}");
+            Console.WriteLine($"País {Pais}");
+        }
+    }
+}
---
+++ Biblioteca/Biblioteca/Program.cs
@@ -0,0 +1,17 @@
+﻿using Biblioteca;
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Livro livro = new Livro("Introdução ao SQL", "9788575225011");
+        livro.Autor = "Thomas Nield";
+        livro.Ano = 2016;
+        livro.Estilo = "Programação";
+        livro.Pais = "Estados Unidos";
+        livro.Paginas = 144;
+
+        livro.ExibeInformacoes();
+    }
+}
